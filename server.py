efa321c79c47ed6adc56f55b3c4df4b076c8bc4d
vitaliko <ivanchenko@gmail.com>
2017-11-26 16:49:50 -0800

    first test case completed

--- server.py
+++ server.py
@@ -1,7 +1,26 @@
 
 from flask import Flask
+from flask import Response
+from flask import json
+
 app = Flask(__name__)
+
+RESERVATIONS = {}
+for hour in ["10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm", "8pm", "9pm", "10pm"]:
+    RESERVATIONS[hour] = {
+    "available": True,
+    "player": None
+}
 
 @app.route("/")
 def index():
     return "Welcome to tennis reservations"
+
+@app.route('/reservations', methods = ['GET'])
+def api_reservations():
+    js = json.dumps(RESERVATIONS)
+
+    resp = Response(js, status=200, mimetype='application/json')
+    resp.headers['Link'] = 'http://localhost:5000'
+
+    return resp
--- tests.py
+++ tests.py
@@ -1,4 +1,3 @@
-
 import unittest
 import requests
 
@@ -10,30 +9,31 @@ for hour in ["10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "
     "player": None
   }
 
-def test_index():
-  response = requests.get(SERVER_URL + "/")
-
-  assert response.status_code == 200
-  assert response.text == "Welcome to tennis reservations"
-
+# def test_index():
+#   response = requests.get(SERVER_URL + "/")
+#
+#   assert response.status_code == 200
+#   assert response.text == "Welcome to tennis reservations"
+#
 def test_get_all_reservations():
-  response = requests.get(SERVER_URL + "/reservations")
+   response = requests.get(SERVER_URL + "/reservations")
 
-  assert response.status_code == 200, "Expected HTTP 200 status"
-  assert response.json() == RESERVATIONS, "Expected JSON payload"
+   assert response.status_code == 200, "Expected HTTP 200 status"
+   assert response.json() == RESERVATIONS, "Expected JSON payload"
 
 def test_get_one_reservation():
   response = requests.get(SERVER_URL + "/reservations?hour=11am")
 
   assert response.status_code == 200, "Expected HTTP 200 status"
-  assert response.json() == RESERVATIONS["11am"], "Expected JSON payload"
+  assert response.json() == RESERVATIONS["11am"], "Expected JSON payload for 11am"
 
-def test_create_new_reservation():
-  payload = {
-    "hour": "11am",
-    "player": "Jesse Wang"
-  }
-  response = requests.post(SERVER_URL + "/reservations", json=payload)
-
-  assert response.status_code == 201, "Expected HTTP 201 status"
-  assert response.json() == payload, "Expected JSON payload"
+#
+# def test_create_new_reservation():
+#   payload = {
+#     "hour": "11am",
+#     "player": "Jesse Wang"
+#   }
+#   response = requests.post(SERVER_URL + "/reservations", json=payload)
+#
+#   assert response.status_code == 201, "Expected HTTP 201 status"
+#   assert response.json() == payload, "Expected JSON payload"
